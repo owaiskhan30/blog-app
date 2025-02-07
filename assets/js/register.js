2dad2debc2c98e8a33baa7ff4b5fc6049372479e
awaiskhan20 <awaiskhan.raees123@gmail.com>
2025-02-07 23:15:07 +0500

    Login Signup Form Updated

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,8 +1,8 @@
-let widget = cloudinary.createUploadWidget({ 
-    cloudName: "dyi9sqwmt", 
-    uploadPreset: "preset1" 
-}, 
-(error, result) => {
-    console.log(error, result);
- });
-  widget.open();
+// let widget = cloudinary.createUploadWidget({ 
+//     cloudName: "dyi9sqwmt", 
+//     uploadPreset: "preset1" 
+// }, 
+// (error, result) => {
+//     console.log(error, result);
+//  });
+//   widget.open();
